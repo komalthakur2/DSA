c220c102504c28384dc3ebc8b46d1b55bde52001
KOMAL KUMARI THAKUR <119059675+komalthakur2@users.noreply.github.com>
2025-08-28 00:55:05 +0530

    Reverse elements

--- Arrays/Reverse elements.java
+++ Arrays/Reverse elements.java
@@ -1,1 +1,28 @@
+import java.util.*;
+public class ReverseArray {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int n = sc.nextInt();
+        System.out.println("Enter the elements of the array:");
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int i = 0, j = n - 1;
+        while (i < j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+        System.out.print("Array after reversing elements:");
+        for (i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+
+}
 
